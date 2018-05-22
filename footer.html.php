84e947bf0e8b5bac7d62ba9142449c6da9a45e6b
LeoLdc <ledocq.leo@laposte.net>
2018-05-22 10:46:40 +0200

    maj

---
+++ footer.html.php
@@ -0,0 +1,3 @@
+<footer>
+<p>  ouyeah</p>
+</footer>
---
+++ header.html.php
@@ -0,0 +1,7 @@
+<head>
+  <meta charset="utf-8">
+    <title>
+      Borderland Les Armes!
+    </title>
+  <link rel="stylesheet" type="text/css" href="css/arme.css">
+</head>
